e1b243fa283c3cffbda3f878c4dcd1a8497513ad
Ehab-Kamil <Ehabkamil2@gmail.com>
2016-04-02 21:34:24 +0200

    Aya Mahmoud Changes

--- src/java/HibernateDao/HProductDao.java
+++ src/java/HibernateDao/HProductDao.java
@@ -57,7 +57,17 @@ public class HProductDao implements DoaInterface<Product> {
 
     @Override
     public int delete(Product bean) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int flag = 0;
+        session = HDBconnect.getInstance().getSession();
+        try {
+            session.beginTransaction();
+            session.delete(bean);
+            session.getTransaction().commit();
+            flag = 1;
+        } catch (Exception e) {
+            flag = 0;
+        }
+        return flag;
     }
 
     @Override
@@ -92,7 +102,7 @@ public class HProductDao implements DoaInterface<Product> {
     public List<Product> search(String keyword) {
 
         session = HDBconnect.getInstance().getSession();
-        
+
         String hql = "from Product p where productName like :keyword";
 //        String keyword = "test";
         Query query = session.createQuery(hql);
