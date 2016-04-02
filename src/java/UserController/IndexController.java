128ebff00b0d25457a998c16da9aa03ac5aaea11
Amira Anis <Amira Anis@192.168.56.1>
2016-04-02 15:39:13 +0200

    Samiraaaaa images of search

--- src/java/UserController/IndexController.java
+++ src/java/UserController/IndexController.java
@@ -20,8 +20,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.imgscalr.Scalr;
-import static org.imgscalr.Scalr.OP_ANTIALIAS;
 
 /**
  *
